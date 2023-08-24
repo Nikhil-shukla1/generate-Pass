913f353077a4cd05bcd4c0757bc57ee2cb0c023b
Nikhil-shukla1 <nikhilshukla311@gmail.com>
2023-08-24 08:44:43 +0530

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const slider = document.querySelector(".slider");
 const lengthDisplay = document.querySelector("[data-len]");
 const passwordDisplay = document.querySelector("[data-passwordDisplay]");//custom 
 const copyBtn = document.querySelector("[copy]");
-const msgBtn = document.querySelector("[CopyMsg]");
+const CopyMsg = document.querySelector("[CopyMsg]");
 const upperCase = document.querySelector("#uppercase");
 const lowerCase = document.querySelector("#lowercase");
 const symbol = document.querySelector("#smybol");
@@ -11,7 +11,7 @@ const indicator = document.querySelector("[data-indicator]");
 const generateBtn = document.querySelector(".generateButton");
 const allcheckBox = document.querySelectorAll("input[type=checkbox]");
 
-const sym = "!@#$%^&*()_+{}|:><?`-=[];',./~";
+const symString = "!@#$%^&*()_+{}|:><?`-=[];',./~";
 let password = "";
 let passwordLength = 10;
 let checkCount=1;
@@ -41,5 +41,51 @@ function generateUpper(){
     return String.fromCharCode(getRandomInteger(65,91));
 }
 function generateSymbol(){
-    return sym.charAt(etRandomInteger(0,sym.length));
+    return symString.charAt(getRandomInteger(0,symString.length));
+}
+
+function calcStrength(){
+    let num = false;
+    let upp = false;
+    let low = false;
+    let sym = false;
+
+    if(upperCase.Checked) upp = true;
+    if(lowerCase.Checked) low = true;
+    if(number.Checked) num = true;
+    if(symbol.Checked) sym = true;
+
+    if(upp && low && (sym || num) && passwordLength>=8){
+        setIndicator("#0f0");
+    }
+    else if(low || num || sym || upp){
+        setIndicator("#ff0");
+    }
+    else{
+        setIndicator("#f00");
+    }
+}
+
+async function copyContent(){
+    try{
+        await navigator.clipboard.writeText(passwordDisplay.value);
+        CopyMsg.innerText = "Copied";
+    }
+    catch(e){
+        CopyMsg.innerText = "failed";
+    }
+    CopyMsg.classList.add("active");//to make copied visible 
+    setTimeout(()=>{
+        CopyMsg.classList.remove("active");
+    },2500);
+}
+
+slider.addEventListener('input',(e)=>{
+    passwordLength = e.target.value;
+    handleSlider();
+})
+function generatePassword(){
+    if(passwordDisplay.value){
+        copyContent();
+    }
 }
